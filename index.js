c761e7712bbb0b82738148e12301c778b7c83fd8
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-08-02 22:26:29 -0300

    feat: req 03/04 : creates and applies input validation on POST /login

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+const { validateEmail, validatePassword,
+  generateToken } = require('./helpers/ValidatingFunctions');
+const { DATA_TALKERS, NOT_FOUND_STATUS, LOGIN_ERROR_STATUS,
+  emailNull, pwNull, emailErr, pwErr } = require('./helpers/constants');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
-
-const DATA_TALKERS = './talker.json';
-const NOT_FOUND_STATUS = 404;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -31,6 +32,18 @@ app.get('/talker/:id', async (req, res) => {
   const talker = JSON.parse(talkers).find((talkr) => talkr.id === +id);
   if (talker) return res.status(HTTP_OK_STATUS).json(talker);
   return res.status(NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  if (!email) return res.status(LOGIN_ERROR_STATUS).json({ message: emailNull });
+  if (!password) return res.status(LOGIN_ERROR_STATUS).json({ message: pwNull });
+  const validEmail = validateEmail(email);
+  const validPassword = validatePassword(password);
+  if (!validEmail) return res.status(LOGIN_ERROR_STATUS).json({ message: emailErr });
+  if (!validPassword) return res.status(LOGIN_ERROR_STATUS).json({ message: pwErr });
+  const token = generateToken();
+  return res.status(HTTP_OK_STATUS).json({ token });
 });
 
 app.listen(PORT, () => {
